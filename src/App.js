41e2c7fe3774e1ebf39bd436abf239ee12c74480
YogevBatito <byogev18185@gmail.com>
2018-11-06 06:56:02 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { googleMaps, loadVenues} from './API'
 import './App.css';
 
 class App extends Component {
+
+  state = {
+    venues: []
+  }
+
+  componentDidMount() {
+    let googleMapsPromise = googleMaps();
+    let fourSquareVenues = loadVenues();
+
+    Promise.all([
+      googleMapsPromise,
+      fourSquareVenues
+  ])
+  .then((data) => {
+    let google = data[0]
+    let venues = data[1].response.venues;
+
+    this.google = google
+    this.markers = [];
+    this.map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: venues[0].location.lat, lng: venues[0].location.lng},
+            zoom: 10
+          });
+
+          venues.map(venue => {
+            let marker = new google.maps.Marker({
+              position: { lat: venue.location.lat, lng: venue.location.lng},
+              map: this.map
+      });
+    })
+  })
+}
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="map"></div>
       </div>
     );
   }
 }
+
+
+
+
+
+
 
 export default App;
